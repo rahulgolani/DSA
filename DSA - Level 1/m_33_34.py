cb1aeb22587b626f64b91c65764f491e16a8b6c0
Rahul Golani <rahulgolani24@live.com>
2023-02-11 15:35:53 +0530

    DSA

---
+++ DSA - Level 1/m_33_34.py
@@ -0,0 +1,12 @@
+#BENJAMIN BULBS
+
+def getOnBulbsAfterFluctuation(n):
+    num=1
+    while num*num<=n:
+        print(num*num)
+        num+=1
+
+
+if __name__ == '__main__':
+    n=20
+    getOnBulbsAfterFluctuation(n)
