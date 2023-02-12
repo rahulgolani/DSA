bb5915aceca38b37fe729c97e5c8de56caa612a5
Rahul Golani <rahulgolani24@live.com>
2023-02-12 13:43:05 +0530

    DSA

---
+++ DSA - Level 1/u_49_50.py
@@ -0,0 +1,12 @@
+def printPattern(n):
+    for i in range(n):
+        for j in range(n):
+            if j==n-i-1:
+                print('*',end='')
+            else:
+                print(' ',end='')
+        print()
+
+if __name__ == '__main__':
+    n=5
+    printPattern(n)
