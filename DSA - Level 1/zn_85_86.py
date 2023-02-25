693063108ef1e69685a3454eba4c5e74188c46b0
Rahul Golani <rahulgolani24@live.com>
2023-02-25 10:46:39 +0530

    DSA

--- DSA - Level 1/zn_85_86.py
+++ DSA - Level 1/zn_85_86.py
@@ -4,7 +4,7 @@ def baseAddition(n1,n2,base):
     carry=0
     power=1
     result=0
-    while n1>0 or n2>0:
+    while n1>0 or n2>0 or carry>0:
         dig1=n1%10 if n1>0 else 0
         dig2=n2%10 if n2>0 else 0
         dig=dig1+dig2+carry
@@ -14,15 +14,15 @@ def baseAddition(n1,n2,base):
         power*=10
         n1=n1//10
         n2=n2//10
-    if carry:
-        result+=carry*power
     return result
 
 
 if __name__ == '__main__':
-    # n1=234
-    # n2=444
-    # base=5
+    n1=234
+    n2=444
+    base=5
+    result=baseAddition(n1,n2,base)
+    print(f"{n1} + {n2} in base {base} is {result}")
     n1=236
     n2=754
     base=8
